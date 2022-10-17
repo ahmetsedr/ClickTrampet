8760797b4ba6289241966442734b4a47fd5eeb5d
ahmetsedr <115935249+ahmetsedr@users.noreply.github.com>
2022-10-17 20:37:33 +0300

    Click Trumpet With Sounds with JS
    Click Trumpet With Sounds with HTML+JS+CSS for fun :smiley:

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function playSound1() {
+    var sound = document.getElementById("audio1");
+    sound.play();
+}
+function playSound2() {
+    var sound = document.getElementById("audio2");
+    sound.play();
+}
+function playSound3() {
+    var sound = document.getElementById("audio3");
+    sound.play();
+}
